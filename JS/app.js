ad62eecfc2e23c0246c047c4eabfc6e957b03cb9
Bryan <btruong10@my.bcit.ca>
2021-03-23 15:29:20 -0700

    finished part 5, pushed completed todo's to completedTodo's array

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,19 @@
-// your JavaScript goes here, you can delete this comment
+let todoList = [
+    
+        {todo: "clone repo for starter code", status: "complete"},
+        {todo: "changing to my repository", status: "complete"},
+        {todo: "working on JS", status: "started"},
+        {todo: "pushed final work to github", status: "started"}
+
+]
+
+let completedTodos = []
+
+for(let i = 0; i < todoList.length; i++){
+    if(todoList[i].status == "complete"){
+        completedTodos.push(todoList[i])
+    }
+}
+
+
+console.log(completedTodos)
